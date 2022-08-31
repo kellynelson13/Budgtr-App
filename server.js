1d250b9d3d6d1c482f29e44db9c64c27d8b6188c
Kelly Nelson <kelnel86@gmail.com>
2022-08-31 17:03:43 -0500

    sets up new and show routes

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const budget = require("./models/budget.js");
+
+app.use(express.urlencoded({ extended: false }))
 
 
 /// ROUTES /////
+//Index
 app.get("/budgtr", function(req, res){
-    res.render("index.ejs")
+    
+    res.render("index.ejs", {
+        budgetData: budget
+    })
+})
+//New
+app.get("/budgtr/new", function(req, res){
+    res.render("new.ejs")
+})
+
+//Show
+app.get("/budgtr/:id", (req, res) => {
+    res.render("show.ejs", {
+        item: budget[req.params.id]
+    })
 })
 
 
